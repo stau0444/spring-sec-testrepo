2696c64ddf756ee525b5947862dca5696a732046
ugo <stau04@gmail.com>
2021-06-27 13:17:33 +0900

    스프링시큐리티 공부 1- gradle을 통한 프로젝트 생성

---
+++ comp/common-utils/src/main/java/com/sp/fc/Person.java
@@ -0,0 +1,14 @@
+package com.sp.fc;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Person {
+    private String name;
+}
---
+++ server/basic-server/src/main/java/com/sp/fc/controller/TestController.java
@@ -0,0 +1,15 @@
+package com.sp.fc.controller;
+
+import lombok.Getter;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @RequestMapping("/")
+    public String home(){
+        return "this is home";
+    }
+}
