7c617093c21bde5ae705adb3c3f446a530300e61
ugo <stau04@gmail.com>
2021-07-03 23:48:11 +0900

    DB 연동 UserDeatils, UserDetailsService 구현

---
+++ comp/user-admin/src/main/java/com/sp/fc/user/service/UserService.java
@@ -0,0 +1,74 @@
+package com.sp.fc.user.service;
+
+import com.sp.fc.user.domain.SpAuthority;
+import com.sp.fc.user.domain.SpUser;
+import com.sp.fc.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class UserService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findSpUserByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException(username));
+    }
+
+    public Optional<SpUser> findUser(String email){
+        return userRepository.findSpUserByEmail(email);
+    }
+
+    public SpUser save(SpUser user){
+        return userRepository.save(user);
+    }
+
+    public void addAuthority(Long userId,String authority){
+        userRepository.findById(userId).ifPresent(user -> {
+            SpAuthority newRole = new SpAuthority(user.getId(),authority);
+            if(user.getAuthorities() == null){
+                HashSet<SpAuthority> authorities = new HashSet<>();
+                authorities.add(newRole);
+                user.setAuthorities(authorities);
+                save(user);
+            }else if(!user.getAuthorities().contains(newRole)){
+                HashSet<SpAuthority> authorities = new HashSet<>();
+                authorities.addAll(user.getAuthorities());
+                authorities.add(newRole);
+                user.setAuthorities(authorities);
+                save(user);
+            }
+        });
+    }
+
+    public void removeAuthority(Long userid , String authority){
+        userRepository.findById(userid).ifPresent(
+                user -> {
+                    if(user.getAuthorities() == null){
+                        return;
+                    }
+                    SpAuthority targetRole = new SpAuthority(userid, authority);
+                    if(user.getAuthorities().contains(targetRole)){
+                        user.setAuthorities(
+                                user.getAuthorities().stream().filter(
+                                        auth -> !(auth.equals(targetRole)))
+                                .collect(Collectors.toSet())
+                        );
+                        save(user);
+                    }
+                }
+        );
+    }
+}
