4c354e120ec4e5f4fda8d0e957d5267b263e025b
ugo <stau04@gmail.com>
2021-07-16 16:18:21 +0900

    ACL,ehCache 사용 권한관리 테스트

---
+++ server/authority-acl/src/main/java/com/sp/fc/web/config/CacheConfig.java
@@ -0,0 +1,20 @@
+package com.sp.fc.web.config;
+
+//@Configuration
+//@EnableCaching
+public class CacheConfig {
+
+//    @Bean
+//    public EhCacheCacheManager cacheManager(){
+//        EhCacheCacheManager cacheManager = new EhCacheCacheManager();
+//        cacheManager.setCacheManager(ehcacheFactoryBean().getObject());
+//        return cacheManager;
+//    }
+//
+//    @Bean
+//    public EhCacheManagerFactoryBean ehcacheFactoryBean() {
+//        EhCacheManagerFactoryBean factoryBean = new EhCacheManagerFactoryBean();
+//        return factoryBean;
+//    }
+
+}
