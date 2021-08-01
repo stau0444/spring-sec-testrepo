74d89172d0f75be59d430a4da548f24848d0fb21
ugo <stau04@gmail.com>
2021-08-01 22:16:46 +0900

    구글 OAuth2 소셜 로그인 테스트

---
+++ server/sns-login-service/src/main/java/com/sp/fc/web/controller/HomeController.java
@@ -0,0 +1,26 @@
+package com.sp.fc.web.controller;
+
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.config.oauth2.client.CommonOAuth2Provider;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthorizationCodeReactiveAuthenticationManager;
+import org.springframework.security.oauth2.client.authentication.OAuth2LoginReactiveAuthenticationManager;
+import org.springframework.security.oauth2.client.oidc.authentication.OidcAuthorizationCodeAuthenticationProvider;
+import org.springframework.security.oauth2.client.oidc.authentication.OidcAuthorizationCodeReactiveAuthenticationManager;
+import org.springframework.security.oauth2.client.web.OAuth2LoginAuthenticationFilter;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("/greeting")
+    @ResponseBody
+    public OAuth2User greeting(@AuthenticationPrincipal OAuth2User auth2User){
+        return auth2User;
+    }
+}
